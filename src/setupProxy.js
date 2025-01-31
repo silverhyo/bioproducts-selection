e9ca30354892464c26bde56f46933e9ab639db7e
silverhyo <silverhyo1@naver.com>
2025-01-31 23:48:49 +0900

    proxy, redirects, package 수정

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.export = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://localhost:8080",
+      changeOrigin: true,
+    })
+  );
+};
